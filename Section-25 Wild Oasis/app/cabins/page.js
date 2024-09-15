a36acab5dc8d3e9e7de2e8fc68de1b65336fb518
custom labels <custom label>
2024-09-15 23:00:16 +0800

    feat: add more pics and delete fetching data

--- Section-25 Wild Oasis/app/cabins/page.js
+++ Section-25 Wild Oasis/app/cabins/page.js
@@ -1,19 +1,19 @@
-import Counter from "../components/Counter"
+import Counter from "@/app/_components/Counter"
 
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users")
-  const data = await res.json()
+export default function Page() {
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  // const data = await res.json()
 
   console.log(data)
   return (
     <div>
       <h1>Cabins pages</h1>
-      <ul>
+      {/* <ul>
         {data.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
-      <Counter users={data} />
+      <Counter users={data} /> */}
     </div>
   )
 }
