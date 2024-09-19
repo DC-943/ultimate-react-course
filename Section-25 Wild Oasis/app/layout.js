04d96404485703a404c9fe38b9a55aed8d56b037
custom labels <custom label>
2024-09-19 12:28:52 +0800

    feat:add profile reservation pages add more css add Image component

--- Section-25 Wild Oasis/app/layout.js
+++ Section-25 Wild Oasis/app/layout.js
@@ -7,6 +7,7 @@ const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" })
 console.log(josefin)
 
 import "@/app/_styles/globals.css"
+import Header from "./_components/Header"
 
 export const metadata = {
   title: {
@@ -21,14 +22,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-800 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-800  text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by the Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 ps-8 py-12 grid ">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   )
