40bb47aef88a95f1323bd8d828e1ffe9ce0986cd
custom labels <custom label>
2024-10-05 23:31:25 +0800

    feat:delete span

--- Section-25 Wild Oasis/app/cabins/loading.js
+++ Section-25 Wild Oasis/app/cabins/loading.js
@@ -4,9 +4,8 @@ export default function Loading() {
   return (
     <div className="grid items-center text-xl text-primary-200 justify-center">
       <Spinner />
-      <span>
-        <p className="">Loading cabin data...</p>
-      </span>
+
+      <p className="">Loading cabin data...</p>
     </div>
   )
 }
